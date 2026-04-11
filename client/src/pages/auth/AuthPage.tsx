53380263150e681a98dd6be26fad763f2741fe97
sandeepsabu <sandeepsabu321@gmail.com>
2026-04-11 11:15:11 +0530

    fix para

--- client/src/pages/auth/AuthPage.tsx
+++ client/src/pages/auth/AuthPage.tsx
@@ -5,7 +5,7 @@ export default function AuthPage() {
   const { pathname } = useParams()
 
   return (
-    <main className="p-6 flex flex-col justify-center items-center h-[80vh]">
+    <main className="p-4 flex flex-col justify-center items-center h-[80vh]">
   <AuthView 
     pathname={pathname} 
     classNames={{
